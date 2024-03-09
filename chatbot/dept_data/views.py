2741aeb390f4dd9a8bd2290d979ee14b44e15e4c
Owusu-Turkson <oteekow@gmail.com>
2024-03-09 17:21:01 +0000

    creating project

---
+++ chatbot/dept_data/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+import json
+
+
+
+# Create your views here.
+# class Files():
+#     def main(self):
+#         return "Hello, world. You're at the dept_data index."
+#     def get_response(intents_list, intents_json):
+#          pass
+#     def predict_class(self, sentence):
+#         pass
+# class Chatbot(APIView):
+#        def post(self, request, *args, **kwargs):
+#            data = request.data
+#            ints= Files().predict_class(data)
+#            res = get_response(ints,intents)
+#            return HttpResponse(json.dumps(data))
+      
+#        pass
